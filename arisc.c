add8e68bf260a218e99bfcfa663c0435e189d7a2
MX-Master <mikhail@vydrenko.ru>
2018-06-29 20:42:42 +0600

    init

---
+++ arisc.c
@@ -0,0 +1,232 @@
+/**
+ * @file    arisc.c
+ *
+ * @brief   ARISC firmware API source
+ *
+ * This test program implements an API to ARISC firmware
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <byteswap.h>
+#include "arisc.h"
+
+
+
+
+// private vars
+
+static struct msg_t * msg_arisc[MSG_MAX_CNT] = {0};
+static struct msg_t * msg_arm[MSG_MAX_CNT] = {0};
+
+static uint32_t *vrt_block_addr;
+
+
+
+
+// public methods
+
+int8_t msg_read(uint8_t type, uint8_t * msg, uint8_t bswap)
+{
+    static uint8_t last = 0;
+    static uint8_t m = 0;
+    static uint8_t i = 0;
+    static uint32_t * link;
+
+    // find next unread message
+    for ( i = MSG_MAX_CNT, m = last; i--; )
+    {
+        // process message only of current type
+        if ( msg_arisc[m]->unread && msg_arisc[m]->type == type )
+        {
+			if ( bswap )
+			{
+                // swap message data bytes for correct reading by ARM
+                link = ((uint32_t*) &msg_arisc[m]->msg);
+                for ( i = msg_arisc[m]->length / 4 + 1; i--; link++ )
+                {
+                    *link = __bswap_32(*link);
+                }
+			}
+
+            // copy message to the buffer
+			memcpy(msg, &msg_arisc[m]->msg, msg_arisc[m]->length);
+
+            // message read
+            msg_arisc[m]->unread = 0;
+            last = m;
+            return msg_arisc[m]->length;
+        }
+
+        ++m;
+        if ( m >= MSG_MAX_CNT ) m = 0;
+    }
+
+    return -1;
+}
+
+
+
+
+/**
+ * @brief   send a message to the ARM cpu
+ *
+ * @param   type    user defined message type (0..0xFF)
+ * @param   msg     pointer to the message buffer
+ * @param   length  the length of a message ( 0 .. (MSG_MAX_LEN-4) )
+ * @param   bswap   0 - if you sending an array of 32bit numbers, 1 - for the text
+ *
+ * @retval   0 (message sent)
+ * @retval  -1 (message not sent)
+ */
+int8_t msg_send(uint8_t type, uint8_t * msg, uint8_t length, uint8_t bswap)
+{
+    static uint8_t last = 0;
+    static uint8_t m = 0;
+    static uint8_t i = 0;
+    static uint32_t * link;
+
+    // find next free message slot
+    for ( i = MSG_MAX_CNT, m = last; i--; )
+    {
+        // sending message
+        if ( !msg_arm[m]->unread )
+        {
+            // copy message to the buffer
+            memset( (uint8_t*)((uint8_t*)&msg_arm[m]->msg + length/4*4), 0, 4);
+            memcpy(&msg_arm[m]->msg, msg, length);
+
+			if ( bswap )
+			{
+                // swap message data bytes for correct reading by ARISC
+                link = ((uint32_t*) &msg_arm[m]->msg);
+                for ( i = length / 4 + 1; i--; link++ )
+                {
+                    *link = __bswap_32(*link);
+                }
+			}
+
+            // set message data
+            msg_arm[m]->type   = type;
+            msg_arm[m]->length = length;
+            msg_arm[m]->unread = 1;
+
+            // message sent
+            last = m;
+            return 0;
+        }
+
+        ++m;
+        if ( m >= MSG_MAX_CNT ) m = 0;
+    }
+
+    // message not sent
+    return -1;
+}
+
+
+
+
+int main(void)
+{
+    int32_t     mem_fd;
+    uint32_t    vrt_offset = 0;
+    off_t       phy_block_addr = 0;
+    int32_t     m = 0;
+    uint8_t     buf[MSG_LEN] = {0};
+
+
+    // open physical memory file
+    if ( (mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0 )
+    {
+       printf("ERROR: can't open /dev/mem file\n");
+       return -1;
+    }
+
+    // calculate phy memory block start
+    vrt_offset = MSG_ARISC_BLOCK_ADDR % PHY_MEM_BLOCK_SIZE;
+    phy_block_addr = MSG_ARISC_BLOCK_ADDR - vrt_offset;
+
+    // make a block of phy memory visible in our user space
+    vrt_block_addr = mmap(NULL, 2*MSG_BLOCK_SIZE, PROT_READ | PROT_WRITE,
+       MAP_SHARED, mem_fd, phy_block_addr);
+
+    // exit program if mmap is failed
+    if (vrt_block_addr == MAP_FAILED)
+    {
+       printf("ERROR: mmap() failed\n");
+       return -1;
+    }
+
+    // adjust offset to correct value
+    vrt_block_addr += (vrt_offset/4);
+
+    // assign messages pointers
+    for ( m = 0; m < MSG_MAX_CNT; ++m )
+    {
+        msg_arisc[m] = (struct msg_t *) (vrt_block_addr + (m * MSG_MAX_LEN)/4);
+        msg_arm[m]   = (struct msg_t *) (vrt_block_addr + (m * MSG_MAX_LEN + MSG_CPU_BLOCK_SIZE)/4);
+    }
+
+    
+
+
+    // setup vars
+    uint8_t t = 0, n = 0;
+
+    // setup pins
+    {
+        // cleanup buffer
+        memset(&buf, 0, MSG_LEN);
+
+        GPIO_MSG_BUF_OUTPUT_MASK(&buf,PA) |= (1U << 15);
+        GPIO_MSG_BUF_OUTPUT_MASK(&buf,PL) |= (1U << 10);
+
+        msg_send(GPIO_MSG_SETUP, (uint8_t*)&buf, GPIO_MSG_SETUP_LEN, 0);
+    }
+
+
+
+
+    for(;;)
+    {
+        n++;
+
+        // 1sec delay
+        usleep(1000000);
+
+        // cleanup buffer
+        memset(&buf, 0, MSG_LEN);
+
+        // toogle pins PA15 and PL10
+        if ( t )
+        {
+            GPIO_MSG_BUF_SET_MASK(&buf,PA)   |= (1U << 15);
+            GPIO_MSG_BUF_CLEAR_MASK(&buf,PL) |= (1U << 10);
+            t = 0;
+            printf("%d: PA15 = 1, PL10 = 0 \n", n);
+        }
+        else
+        {
+            GPIO_MSG_BUF_CLEAR_MASK(&buf,PA) |= (1U << 15);
+            GPIO_MSG_BUF_SET_MASK(&buf,PL)   |= (1U << 10);
+            t = 1;
+            printf("%d: PA15 = 0, PL10 = 1 \n", n);
+        }
+
+        // send a message
+        msg_send(GPIO_MSG_SET, (uint8_t*)&buf, GPIO_MSG_SET_LEN, 0);
+    }
+
+
+
+
+    munmap(vrt_block_addr, 2*MSG_BLOCK_SIZE);
+
+    return 0;
+}
---
+++ arisc.h
@@ -0,0 +1,180 @@
+/**
+ * @file    arisc.h
+ *
+ * @brief   ARISC firmware API header
+ *
+ * This test program implements an API to ARISC firmware
+ */
+
+#ifndef _ARISC_H
+#define _ARISC_H
+
+#include <stdint.h>
+
+
+
+
+#define PHY_MEM_BLOCK_SIZE      4096
+
+#define SRAM_A2_SIZE            (48*1024)
+#define SRAM_A2_ADDR            0x00040000 ///< for ARM use 0x00040000
+#define ARISC_CONF_SIZE         2048
+#define ARISC_CONF_ADDR         (SRAM_A2_ADDR + SRAM_A2_SIZE - ARISC_CONF_SIZE)
+
+#define MSG_BLOCK_SIZE          4096
+#define MSG_BLOCK_ADDR          (ARISC_CONF_ADDR - MSG_BLOCK_SIZE)
+
+#define MSG_CPU_BLOCK_SIZE      2048
+#define MSG_ARISC_BLOCK_ADDR    (MSG_BLOCK_ADDR + 0)
+#define MSG_ARM_BLOCK_ADDR      (MSG_BLOCK_ADDR + MSG_CPU_BLOCK_SIZE)
+
+#define MSG_MAX_CNT             8
+#define MSG_MAX_LEN             (MSG_CPU_BLOCK_SIZE / MSG_MAX_CNT)
+#define MSG_LEN                 (MSG_MAX_LEN - 4)
+
+#define MSG_RECV_CALLBACK_CNT   32
+
+#pragma pack(push, 1)
+struct msg_t
+{
+    uint8_t length;
+    uint8_t type;
+    uint8_t locked; // actually not used at this moment
+    uint8_t unread;
+    uint8_t msg[MSG_LEN];
+};
+#pragma pack(pop)
+
+
+
+
+#define GPIO_PORTS_CNT          8   ///< number of GPIO ports
+#define GPIO_PINS_CNT           32  ///< number of GPIO port pins
+
+/// the GPIO port names
+enum { PA, PB, PC, PD, PE, PF, PG, PL };
+
+/// the GPIO pin states
+enum { LOW, HIGH };
+
+/// the message types
+enum
+{
+    GPIO_MSG_SETUP = 0x10,
+    GPIO_MSG_GET,
+    GPIO_MSG_SET,
+};
+
+/// the message data sizes
+#define GPIO_MSG_BUF_LEN        MSG_LEN
+#define GPIO_MSG_SETUP_LEN      (4*2*GPIO_PORTS_CNT)
+#define GPIO_MSG_GET_LEN        (4*GPIO_PORTS_CNT)
+#define GPIO_MSG_SET_LEN        (4*2*GPIO_PORTS_CNT)
+
+/// the message data access
+#define GPIO_MSG_BUF_INPUT_MASK(LINK,PORT)      (*((uint32_t*)(LINK) + PORT))
+#define GPIO_MSG_BUF_OUTPUT_MASK(LINK,PORT)     (*((uint32_t*)(LINK) + PORT + GPIO_PORTS_CNT))
+
+#define GPIO_MSG_BUF_SET_MASK(LINK,PORT)        (*((uint32_t*)(LINK) + PORT))
+#define GPIO_MSG_BUF_CLEAR_MASK(LINK,PORT)      (*((uint32_t*)(LINK) + PORT + GPIO_PORTS_CNT))
+
+#define GPIO_MSG_BUF_STATE(LINK,PORT)           (*((uint32_t*)(LINK) + PORT))
+
+
+
+
+#define PULSGEN_CH_CNT      32  ///< maximum number of pulse generator channels
+#define PULSGEN_MAX_DUTY    100 ///< maximum percent of pulse duty cycle (255 is max)
+#define PULSGEN_MAX_PERIOD  (UINT32_MAX/(TIMER_FREQUENCY/1000000))
+
+/// messages types
+enum
+{
+    PULSGEN_MSG_PIN_SETUP = 0x20,
+    PULSGEN_MSG_TASK_SETUP,
+    PULSGEN_MSG_TASK_ABORT,
+    PULSGEN_MSG_TASK_STATE,
+    PULSGEN_MSG_TASK_TOGGLES
+};
+
+#define PULSGEN_MSG_BUF_LEN             MSG_LEN
+#define PULSGEN_MSG_CH_CNT              12
+#define PULSGEN_MSG_PIN_SETUP_LEN       (4*4*PULSGEN_MSG_CH_CNT)
+#define PULSGEN_MSG_TASK_SETUP_LEN      (5*4*PULSGEN_MSG_CH_CNT)
+#define PULSGEN_MSG_TASK_ABORT_LEN      (4)
+#define PULSGEN_MSG_TASK_STATE_LEN      (4)
+#define PULSGEN_MSG_TASK_TOGGLES_LEN    (4*PULSGEN_MSG_CH_CNT)
+
+/// the message data access
+#define PULSGEN_MSG_BUF_CHANNEL_ID(LINK,SLOT)       (*((uint32_t*)(LINK) + SLOT))
+#define PULSGEN_MSG_BUF_PORT(LINK,SLOT)             (*((uint32_t*)(LINK) + SLOT + 1*PULSGEN_MSG_CH_CNT))
+#define PULSGEN_MSG_BUF_PIN(LINK,SLOT)              (*((uint32_t*)(LINK) + SLOT + 2*PULSGEN_MSG_CH_CNT))
+#define PULSGEN_MSG_BUF_INVERTED(LINK,SLOT)         (*((uint32_t*)(LINK) + SLOT + 3*PULSGEN_MSG_CH_CNT))
+
+#define PULSGEN_MSG_BUF_PERIOD(LINK,SLOT)           (*((uint32_t*)(LINK) + SLOT + 1*PULSGEN_MSG_CH_CNT))
+#define PULSGEN_MSG_BUF_DELAY(LINK,SLOT)            (*((uint32_t*)(LINK) + SLOT + 2*PULSGEN_MSG_CH_CNT))
+#define PULSGEN_MSG_BUF_TOGGLES(LINK,SLOT)          (*((uint32_t*)(LINK) + SLOT + 3*PULSGEN_MSG_CH_CNT))
+#define PULSGEN_MSG_BUF_DUTY(LINK,SLOT)             (*((uint32_t*)(LINK) + SLOT + 4*PULSGEN_MSG_CH_CNT))
+
+#define PULSGEN_MSG_BUF_CHANNELS_MASK(LINK)         (*((uint32_t*)(LINK)))
+
+#define PULSGEN_MSG_BUF_TOGGLES_MADE(LINK,SLOT)     (*((uint32_t*)(LINK) + SLOT))
+
+
+
+
+#define ENCODER_CH_CNT 8  ///< maximum number of encoder counter channels
+
+/// a channel parameters
+struct encoder_ch_t
+{
+    uint8_t     enabled;
+    uint8_t     using_B;
+    uint8_t     using_Z;
+
+    uint8_t     port[3];
+    uint8_t     pin[3];
+    uint8_t     state[3];
+
+    int32_t     counts;
+    uint8_t     AB_state;
+};
+
+enum { PHASE_A, PHASE_B, PHASE_Z };
+enum { PH_A, PH_B, PH_Z };
+
+/// messages types
+enum
+{
+    ENCODER_MSG_PINS_SETUP = 0x30,
+    ENCODER_MSG_SETUP,
+    ENCODER_MSG_COUNTS,
+    ENCODER_MSG_ENABLE,
+    ENCODER_MSG_RESET
+};
+
+#define ENCODER_MSG_BUF_LEN             MSG_LEN
+#define ENCODER_MSG_PINS_SETUP_LEN      (4*6*ENCODER_CH_CNT)
+#define ENCODER_MSG_SETUP_LEN           (4*3*ENCODER_CH_CNT)
+#define ENCODER_MSG_COUNTS_LEN          (4*ENCODER_CH_CNT)
+#define ENCODER_MSG_ENABLE_LEN          (4)
+#define ENCODER_MSG_RESET_LEN           (4)
+
+/// the message data access
+#define ENCODER_MSG_BUF_PORT(LINK,CH,PHASE)     (*((uint32_t*)(LINK) + 6*CH + PHASE))
+#define ENCODER_MSG_BUF_PIN(LINK,CH,PHASE)      (*((uint32_t*)(LINK) + 6*CH + PHASE + 3))
+
+#define ENCODER_MSG_BUF_ENABLED(LINK,CH)        (*((uint32_t*)(LINK) + 3*CH))
+#define ENCODER_MSG_BUF_USING_B(LINK,CH)        (*((uint32_t*)(LINK) + 3*CH + 1))
+#define ENCODER_MSG_BUF_USING_Z(LINK,CH)        (*((uint32_t*)(LINK) + 3*CH + 2))
+
+#define ENCODER_MSG_BUF_COUNTS(LINK,CH)         (*((uint32_t*)(LINK) + CH))
+
+#define ENCODER_MSG_BUF_ENABLE(LINK)            (*((uint32_t*)(LINK)))
+
+#define ENCODER_MSG_BUF_RESET(LINK)             (*((uint32_t*)(LINK)))
+
+
+
+
+#endif
